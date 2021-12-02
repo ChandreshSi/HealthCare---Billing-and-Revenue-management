983cb803cc81a2eade79d4c9750a689e2a537efb
Chandresh Sikarwar <cs62@njit.edu>
2021-12-01 19:57:50 -0500

    Missed checkin CPTGroup

---
+++ src/main/java/com/healthcare/billing/model/CPTGroup.java
@@ -0,0 +1,51 @@
+package com.healthcare.billing.model;
+
+import java.util.List;
+
+public class CPTGroup {
+    private String id;
+    private String type;
+    private String description;
+    private String additionalInfo;
+    private List<CPT> codes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+
+    public List<CPT> getCodes() {
+        return codes;
+    }
+
+    public void setCodes(List<CPT> codes) {
+        this.codes = codes;
+    }
+}

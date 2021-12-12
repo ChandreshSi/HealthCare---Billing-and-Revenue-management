5a236bcb7edb91cfd13492a846343230683fd21d
Chandresh Sikarwar <cs62@njit.edu>
2021-12-12 17:17:28 -0500

    port change

--- src/main/java/com/healthcare/billing/constants/BillingConstant.java
+++ src/main/java/com/healthcare/billing/constants/BillingConstant.java
@@ -2,6 +2,6 @@ package com.healthcare.billing.constants;
 
 public interface BillingConstant {
     String CURRENCY_KEY = "system.config.currency";
-    String PATIENT_MOCK_URL = "http://127.0.0.1:5000/mock/patient";
-    String INSURER_MOCK_URL = "http://127.0.0.1:5000/mock/insurer";
+    String PATIENT_MOCK_URL = "http://127.0.0.1:9000/mock/patient";
+    String INSURER_MOCK_URL = "http://127.0.0.1:9000/mock/insurer";
 }
